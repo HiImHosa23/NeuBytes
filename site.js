dfa698c0c4f70d3d37be3274a592be6f2253ae4f
kkingston32 <126709063+kkingston32@users.noreply.github.com>
2023-08-23 15:05:01 -0600

    Add files via upload

---
+++ site.js
@@ -0,0 +1,18 @@
+const myDiv = document.getElementById("myDiv")
+let inputCount = 0;
+
+// do client side things here!
+let handleButtonClick = () => {
+    console.log("Button clicked!");
+
+    inputCount++;
+
+    //let oldHtml = myDiv.innerHTML;
+
+    input = "Ingredient" + inputCount
+
+
+    myDiv.innerHTML = myDiv.innerHTML + '<br>'+ '<input type="text" id="ingredients" name=' + input +  ' placeholder= "New Ingredient" > <br>';
+
+    
+};
