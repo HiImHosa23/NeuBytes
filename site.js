66f4c26f409251948e920e2de7d26f0def9a6250
Keyana L. LaStrap <123585901+iiKeyana@users.noreply.github.com>
2023-08-25 15:33:04 -0600

    Update site.js

--- site.js
+++ site.js
@@ -1,3 +1,61 @@
+var log = document.getElementById('login');
+var reg = document.getElementById('register');
+var btn = document.getElementById('btn');
+
+function login() {
+    log.style.left = "27px";
+    reg.style.right = "-350px";
+    btn.style.left = "0px";
+
+}
+
+function register() {
+    log.style.left = "-350px";
+    reg.style.right = "25px";
+    btn.style.left = "150px";
+
+}
+
+// View password
+
+function myLogPassword() {
+    var password = document.getElementById('logPassword');
+    var eye = document.getElementById('eye');
+    var eyeSlash = document.getElementById('eye-slash');
+
+    if(password.type === "password") {
+        password.type = "text"
+        eye.style.opacity = "0";
+        eyeSlash.style.opacity = "1";
+
+    }else {
+        password.type = "password"
+        eye.style.opacity = "1";
+        eyeSlash.style.opacity = "0";
+
+    }
+
+}
+
+function myRegPassword() {
+    var password = document.getElementById('regPassword');
+    var eye = document.getElementById('eye-2');
+    var eyeSlash = document.getElementById('eye-slash-2');
+
+    if(password.type === "password") {
+        password.type = "text"
+        eye.style.opacity = "0";
+        eyeSlash.style.opacity = "1";
+
+    }else {
+        password.type = "password"
+        eye.style.opacity = "1";
+        eyeSlash.style.opacity = "0";
+
+    }
+
+}
+
 const myDiv = document.getElementById("myDiv")
 let inputCount = 0;
 
